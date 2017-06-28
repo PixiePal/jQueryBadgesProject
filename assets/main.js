1e776b6e4b58a6af61f2381e45560cf69e407531
PixiePal <orsolya.tothgati@gmail.com>
2017-06-28 11:55:52 +0200

    Adds badge list with title, image and link to course.

--- assets/main.js
+++ assets/main.js
@@ -1,5 +1,34 @@
-$(function() {
+$(function () {
 
-  // your code will go here
+	$.ajax({
+		url: "https://www.codeschool.com/users/1917482.json",
+		dataType: 'jsonp',
+		success: function (response) {
+			var completedCourses = response.courses.completed;
+			completedCourses.forEach(function (course) {
+				var newCourseElement = $('<div>', {
+					"class": 'course'
+				});
+				newCourseElement.append(
+					$('<h3>', {
+						"text": course.title
+					}));
+
+				newCourseElement.append(
+					$('<img>', {
+						"src": course.badge
+					}));
+				newCourseElement.append(
+					$('<a>', {
+						"href": course.url,
+						"target": '_blank',
+						"class": "btn btn-primary",
+						"text": "See Course"
+					}));
+
+				$('#badges').append(newCourseElement);
+			});
+		}
+	});
 
 });
